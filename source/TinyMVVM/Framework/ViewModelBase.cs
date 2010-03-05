4d17ca7584878ac9558ff4fdaf19ee52091c1956
goeran.hansen <goeran.hansen@87bf65f0-dce6-11de-95c9-073ad895b44c>
2010-03-05 13:28:15 +0000

    Added header to files with copyright info. Now I can reuse some source files in other projects
    git-svn-id: http://tinymvvm.googlecode.com/svn/trunk@18 87bf65f0-dce6-11de-95c9-073ad895b44c

--- source/TinyMVVM/Framework/ViewModelBase.cs
+++ source/TinyMVVM/Framework/ViewModelBase.cs
@@ -1,9 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region Copyright
+// <copyright>
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//  
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//  
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// </copyright> 
+// 
+// <contactinfo>
+//  The project webpage is located at http://tinymvvm.googlecode.com which contains all the  neccessary information. You might also find more information on Gøran's blog:  http://blog.goeran.no.
+// </contactinfo>
+// 
+// <author>Gøran Hansen</author>
+// <email>mail@goeran.no</email>
+// <date>2010-02-01</date>
+// 
+#endregion
+
+using System;
 using System.ComponentModel;
-using TinyMVVM.Framework.Internal;
 
 namespace TinyMVVM.Framework
 {
