3133223d64c7764566a01e2395946ba90cb040da
goeran.hansen <goeran.hansen@87bf65f0-dce6-11de-95c9-073ad895b44c>
2010-03-17 23:48:44 +0000

    added dymaic object
    git-svn-id: http://tinymvvm.googlecode.com/svn/trunk@28 87bf65f0-dce6-11de-95c9-073ad895b44c

---
+++ source/TinyMVVM/Framework/Internal/DynamicObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TinyMVVM.Framework.Internal
+{
+	internal class DynamicObject
+	{
+		private BindingFlags instanceFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+		private readonly object instance;
+		private readonly Type instanceType;
+
+		public DynamicObject(Object instance)
+		{
+			this.instance = instance;
+			this.instanceType = instance.GetType();
+		}
+
+		public bool MethodExist(string name)
+		{
+			var methods = instanceType.GetMethods(instanceFlags);
+			return methods.Where(m => m.Name.Equals(name)).Count() > 0;
+		}
+
+		public Object InvokeMethod(string name)
+		{
+			var method = GetMethod(name);
+
+			if (method != null)
+				return method.Invoke(instance, null);
+			else
+				return null;
+		}
+
+		private MethodInfo GetMethod(string name)
+		{
+			var methods = instanceType.GetMethods(instanceFlags);
+			return methods.Where(m => m.Name.Equals(name)).SingleOrDefault();
+		}
+
+		public MethodInfo[] GetMethods()
+		{
+			return instanceType.GetMethods(instanceFlags);
+		}
+
+		public PropertyInfo[] GetProperties()
+		{
+			return instanceType.GetProperties(instanceFlags);
+		}
+
+	}
+}
