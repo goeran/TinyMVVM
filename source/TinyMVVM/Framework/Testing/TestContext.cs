f182fd3e314d249ea86e62ee61290ff255645eab
goeran.hansen <goeran.hansen@87bf65f0-dce6-11de-95c9-073ad895b44c>
2010-03-28 19:39:13 +0000

    Added file I forgot
    git-svn-id: http://tinymvvm.googlecode.com/svn/trunk@40 87bf65f0-dce6-11de-95c9-073ad895b44c

---
+++ source/TinyMVVM/Framework/Testing/TestContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+
+namespace TinyMVVM.Framework.Testing
+{
+    public abstract class TestContext
+    {
+        protected Mock<T> GetFakeFor<T>() where T : class
+        {
+            return ServiceLocator.Instance.GetInstance<Mock<T>>();
+        }
+
+    }
+}
