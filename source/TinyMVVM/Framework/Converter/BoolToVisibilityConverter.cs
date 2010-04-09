d4f005786fcd389b88f37c1ec6b99a5a5ebb6134
goeran.hansen <goeran.hansen@87bf65f0-dce6-11de-95c9-073ad895b44c>
2010-04-09 13:36:40 +0000

    Added first value converter
    git-svn-id: http://tinymvvm.googlecode.com/svn/trunk@65 87bf65f0-dce6-11de-95c9-073ad895b44c

---
+++ source/TinyMVVM/Framework/Converter/BoolToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TinyMVVM.Framework.Converter
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                throw new ArgumentException("Invalid value type - must be a boolean");
+
+            var val = (bool) value;
+
+            if (val == true)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
