284243c326ea18c0d3919107a179331a045178cc
goeran <mail@goeran.no>
2010-06-15 17:13:52 +0200

    started experimenting with LinFu. Testing out the possibilities using DynamicProxies

---
+++ source/TinyMVVM.VSIntegration.Tests/Learning/LinFuLearningTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Dynamic;
+using LinFu.AOP.Cecil;
+using LinFu.AOP.Interfaces;
+using LinFu.Proxy;
+using LinFu.Proxy.Interfaces;
+using NUnit.Framework;
+
+namespace TinyMVVM.VSIntegration.Tests.Learning
+{
+    [TestFixture]
+    public class LinFuLearningTests
+    {
+        [Test]
+        public void How_to_create_a_dynamic_proxy()
+        {
+            var factory = new ProxyFactory();
+            var proxy = new PersonProxy();
+            var person = factory.CreateProxy<Person>(proxy as IInterceptor);
+
+            person.Hello();
+            person.Greeting();
+            Console.WriteLine("RetValue: Foo");
+        }
+    }
+
+    public class Person
+    {
+        public virtual void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public virtual void Greeting()
+        {
+            Console.WriteLine("Greeting");
+        }
+
+        public virtual string Foo(string retValue)
+        {
+            Console.WriteLine("Foo");
+            return retValue;
+        }
+    }
+
+    public class PersonProxy : Person, IInterceptor
+    {
+        public PersonProxy()
+        {
+        }
+
+        public object Intercept(IInvocationInfo info)
+        {
+            Console.WriteLine("Intercept: " + info.TargetMethod.Name);
+            return info.TargetMethod.Invoke(this, info.Arguments);
+        }
+    }
+}
---
+++ source/TinyMVVM.VSIntegration/Internal/Repositories/SolutionRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using TinyMVVM.TinyMVVM_VSIntegration.Internal.Factories;
+using TinyMVVM.TinyMVVM_VSIntegration.Internal.Model;
+
+namespace TinyMVVM.TinyMVVM_VSIntegration.Internal.Repositories
+{
+    internal class SolutionRepository 
+    {
+        private EnvDTE.DTE dte;
+        private IModelFactory factory;
+
+        public SolutionRepository(EnvDTE.DTE dte, IModelFactory factory)
+        {
+            this.factory = factory;
+            this.dte = dte;
+        }
+
+        public Solution Get(string solutionFilePath)
+        {
+            var solution = factory.NewSolution();
+            solution.Path = dte.Solution.FullName;
+
+            foreach (var proj in solution.Projects)
+            {
+                var project = factory.NewProject();
+                project.Path = proj.Path;
+                proj.Name = proj.Name;
+            }
+
+            return solution;
+        }
+    }
+}
