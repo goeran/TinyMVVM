aba78099e76b0fffc152cfd8caa793e181354785
goeran <mail@goeran.no>
2010-06-27 20:25:25 +0200

    Started working on hosting IronRuby to make it easier to create DSLs

---
+++ source/TinyMVVM.VSIntegration.Tests/Learning/HostIronRubyTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using IronRuby.Builtins;
+using Microsoft.Scripting.Hosting;
+using NUnit.Framework;
+
+namespace TinyMVVM.VSIntegration.Tests.Learning
+{
+    [TestFixture]
+    public class HostIronRubyTests
+    {
+        private ScriptEngine scriptEngine;
+        private ScriptScope scope;
+
+        [SetUp]
+        public void Setup()
+        {
+            scriptEngine = IronRuby.Ruby.CreateEngine();
+            scope = scriptEngine.CreateScope();
+        }
+
+        [Test]
+        public void How_to_host_the_runtime()
+        {
+            var script = scriptEngine.CreateScriptSourceFromString("def haldis()\n \t \nend\n { \"build views?\" => false }").Compile();
+
+            var dynamic = script.Execute();
+
+            Console.WriteLine(dynamic.ToString());
+        }
+
+        [Test]
+        [Ignore("Don't know how to get access to the variable in script")]
+        public void How_to_manipulate_a_parameter_in_script()
+        {
+            scope.SetVariable("aVar", 1);
+            var script = scriptEngine.CreateScriptSourceFromString("GetVariable(\"aVar\")");
+
+            var dynamic = script.Execute();
+
+            Console.WriteLine(dynamic.ToString());
+        }
+
+        [Test]
+        public void How_to_load_script_from_file()
+        {
+            var scriptPath = Path.Combine(Environment.CurrentDirectory, "Learning", "Script", "config.rb");
+            var script = scriptEngine.CreateScriptSourceFromFile(scriptPath).Compile();
+
+            var config = script.Execute() as Hash;
+
+            Console.WriteLine("output: {0}", config);
+
+            if (config is Hash)
+            {
+                foreach (var key in config.Keys)
+                {
+                    Console.WriteLine("{0} = {1}", key, config[key]);
+                }
+            }
+        }
+    }
+}
---
+++ source/TinyMVVM.VSIntegration.Tests/Learning/Script/config.rb
@@ -0,0 +1,32 @@
+﻿flag = true
+
+@result = {
+	"views" => true,
+	"controllers" => true,
+	"partial ViewModels" => true,
+	"unit tests" => true
+}
+
+def configure
+	yield
+end
+
+def not
+	yield
+end
+
+def generate(what)
+	item = @result.find do |k,v| k == what end
+	if item != nil
+		@result[what] = false
+	end
+end
+
+configure do
+	not generate "views"
+	not generate "controllers"
+	generate "partial ViewModels"
+	generate "unit tests"
+end
+
+@result
