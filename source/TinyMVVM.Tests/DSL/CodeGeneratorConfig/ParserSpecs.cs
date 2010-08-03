bbd377170c493cbe2166ad37b248938e684bec0b
goeran <mail@goeran.no>
2010-08-03 19:50:43 +0200

    added some missing files

---
+++ source/TinyMVVM.Tests/DSL/CodeGeneratorConfig/ParserSpecs.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TinyBDD.Dsl.GivenWhenThen;
+using TinyBDD.Specification.NUnit;
+using TinyMVVM.DSL.Framework;
+using TinyMVVM.DSL.TextParser;
+using Parser = TinyMVVM.DSL.CodeGeneratorConfig.Parser;
+
+namespace TinyMVVM.Tests.DSL.CodeGeneratorConfig
+{
+    class ParserSpecs
+    {
+        [TestFixture]
+        public class When_Parse_without_valid_args : Shared
+        {
+            [SetUp]
+            public void Setup()
+            {
+                Given(Parser_is_created);
+
+                When("parse");
+            }
+
+            [Test]
+            public void assure_LoadingStrategy_arg_is_valided()
+            {
+                Then(() =>
+                    this.ShouldThrowException<ArgumentNullException>(() =>
+                        parser.Parse(null)));
+            }
+        }
+
+        [TestFixture]
+        public class When_Parse_valid_code : Shared
+        {
+            [SetUp]
+            public void Setup()
+            {
+                Given(Parser_is_created);
+                And("Code is valid", () =>
+                    code = "configure do\n" +
+                           "\t generate \"views\" \n" +
+                           "\t generate \"controllers\" \n" +
+                           "end");
+
+                When(Parse);
+            }
+
+            [Test]
+            public void assure_SemanticModel_is_returned()
+            {
+                Then(() =>
+                     semanticModel.ShouldNotBeNull());
+            }
+
+            [Test]
+            public void assure_configuration_key_is_parsed()
+            {
+                Then(() =>
+                    semanticModel.Contains("views").ShouldBeTrue());
+            }
+
+            [Test]
+            public void assure_several_configuration_keys_can_be_parsed()
+            {
+                Then(() =>
+                {
+                    semanticModel.Contains("controllers").ShouldBeTrue();
+                    semanticModel.Contains("views").ShouldBeTrue();
+                });
+            }
+        }
+
+        [TestFixture]
+        public class When_Parse_invalid_code : Shared
+        {
+            [SetUp]
+            public void Setup()
+            {
+                Given(Parser_is_created);
+                And("Code is invalid", () =>
+                    code = "configurea do\n" +
+                           "end\n");
+
+                When("Parse");
+            }
+
+            [Test]
+            public void assure_InvalidSyntaxException_is_thrown()
+            {
+                Then(() =>
+                    this.ShouldThrowException<InvalidSyntaxException>(() =>
+                        Parse(), ex =>
+                            Console.WriteLine(ex.Message)));
+            }
+        }
+
+        [TestFixture]
+        public class When_Parse_code_that_opens_a_file : Shared
+        {
+        }
+
+        public class Shared : NUnitScenarioClass
+        {
+            protected static Parser parser;
+            protected static string code;
+            protected static HashSet<string> semanticModel;
+
+            protected Context Parser_is_created = () =>
+            {
+                parser = new Parser();
+            };
+
+            protected When Parse = () =>
+            {
+                semanticModel = parser.Parse(new InlineCode(code));
+            };
+        }
+    }
+}
---
+++ source/TinyMVVM/DSL/CodeGeneratorConfig/Parser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronRuby.Builtins;
+using Microsoft.Scripting.Hosting;
+using TinyMVVM.DSL.Framework;
+using TinyMVVM.DSL.TextParser;
+
+namespace TinyMVVM.DSL.CodeGeneratorConfig
+{
+    public class Parser
+    {
+        private ScriptEngine scriptEngine = IronRuby.Ruby.CreateEngine();
+
+        public HashSet<string> Parse(ICodeLoader loadingStrategy)
+        {
+            if (loadingStrategy == null) throw new ArgumentNullException();
+
+            var code = string.Format("{0}\n{1}\n@config",
+                GenerateConfigDslCode(),
+                loadingStrategy.Load());
+
+            Hash result = null;
+            try
+            {
+                result = ParseCode(code);
+            }
+            catch (MissingMethodException ex)
+            {
+                throw new InvalidSyntaxException(string.Format("Invalid syntax. {0}", ex.Message), ex);
+            }
+
+            return ConvertResultToSemanticModel(result);
+        }
+
+        private string GenerateConfigDslCode()
+        {
+            var code = new StringBuilder();
+            code.Append("a = \"\"\n");
+            code.Append("@config = {} \n");
+            code.Append("\n");
+            code.Append("def configure \n");
+            code.Append("\t yield \n");
+            code.Append("end \n");
+            code.Append("\n");
+            code.Append("def generate(what)\n");
+            code.Append("\t @config[what] = true \n");
+            code.Append("end \n");
+
+            return code.ToString();
+        }
+
+        private Hash ParseCode(string code)
+        {
+            Hash result = null;
+
+            result = scriptEngine.CreateScriptSourceFromString(code).Compile().Execute<Hash>();
+
+            return result;
+        }
+
+        private HashSet<string> ConvertResultToSemanticModel(Hash result)
+        {
+            var semanticModel = new HashSet<string>();
+
+            if (result != null)
+            {
+                foreach (var item in result)
+                {
+                    semanticModel.Add(item.Key.ToString());
+                }
+            }
+
+            return semanticModel;
+        }
+    }
+}
